59c7632be42ed16c46a21d3eb076cefde551aa11
Anna Ionova <echoespond@gmail.com>
2019-01-23 23:57:24 -0500

    initial app setup commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// below is a named export, so we need curly braces
+// // for the default export we dont need them
+// import { selectSong } from '../actions';
+
+const App = () => {
+  return <div>App</div>;
+};
+
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Fresh Feeling', duration: '3:15' },
+    { title: 'Salmon Song', duration: '2:30' },
+    { title: 'Meds', duration: '1:23'}
+  ];
+};
